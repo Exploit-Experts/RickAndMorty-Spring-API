3ba02430d59a2b7cb320878efddb8c939bfb6b0b
Leandro Oliveira <leo.rodrigues17@hotmail.com>
2025-03-08 17:33:27 -0300

    fix: Changes to character limit of models.

--- src/main/java/com/rickmorty/Models/CharacterModel.java
+++ src/main/java/com/rickmorty/Models/CharacterModel.java
@@ -1,15 +1,52 @@
 package com.rickmorty.Models;
 
+import com.rickmorty.enums.LifeStatus;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "characters")
 public class CharacterModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "character_status", nullable = false)
+    private LifeStatus status;
+
+    @Column(nullable = false, length = 30)
+    private String species;
+
+    @Column(name = "character_type", nullable = false, length = 15)
+    private String characterType;
+
+    @Column(nullable = false, length = 10)
+    private String gender;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private LocationModel locationModel;
+
+    @ManyToMany
+    @JoinTable(
+            name = "character_episodes",
+            joinColumns = @JoinColumn(name = "character_id"),
+            inverseJoinColumns = @JoinColumn(name = "episode_id")
+    )
+    private List<EpisodeModel> episodes;
 
 }
--- src/main/java/com/rickmorty/Models/LocationModel.java
+++ src/main/java/com/rickmorty/Models/LocationModel.java
@@ -1,15 +1,35 @@
 package com.rickmorty.Models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "locations")
 public class LocationModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 15)
+    private String dimension;
+
+    @Column(name = "location_type", nullable = false, length = 20)
+    private String LocationType;
+
+    @OneToMany(mappedBy = "locationModel", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CharacterModel> characters;
 
 }
