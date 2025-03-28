bfe8f7ed2d55ab99492a22d9ba7f5c244013a1c0
Jonathan <jonathanw.rocha@unifacol.edu.br>
2025-03-27 22:47:41 -0300

    increasing activity

--- src/main/java/com/rickmorty/DTO/CharacterDto.java
+++ src/main/java/com/rickmorty/DTO/CharacterDto.java
@@ -2,12 +2,16 @@ package com.rickmorty.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 import java.util.List;
 import com.rickmorty.DTO.CharacterDto;
+import com.rickmorty.enums.Gender;
+import com.rickmorty.enums.LifeStatus;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CharacterDto(
-        @JsonProperty("id") int id,
+        @JsonProperty("id") Long id,
         @JsonProperty("name") String name,
         @JsonProperty("status") String status,
         @JsonProperty("species") String species,
@@ -17,4 +21,6 @@ public record CharacterDto(
         @JsonProperty("episode") List<String> episode,
         @JsonProperty("location") LocationCharacterDto location
 
-) {}
+) {
+
+}
--- src/main/java/com/rickmorty/Repository/CharacterRepository.java
+++ src/main/java/com/rickmorty/Repository/CharacterRepository.java
@@ -1,9 +1,31 @@
 package com.rickmorty.Repository;
 
 import com.rickmorty.Models.CharacterModel;
+import com.rickmorty.enums.Gender;
+import com.rickmorty.enums.LifeStatus;
+import com.rickmorty.enums.Species;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<CharacterModel, Long> {
+
+    @Query("SELECT c FROM CharacterModel c WHERE " +
+            "(:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND " +
+            "(:status IS NULL OR c.status = :status) AND " +
+            "(:species IS NULL OR LOWER(c.species) LIKE LOWER(CONCAT('%', :species, '%'))) AND " +
+            "(:characterType IS NULL OR LOWER(c.characterType) LIKE LOWER(CONCAT('%', :characterType, '%'))) AND " +
+            "(:gender IS NULL OR c.gender = :gender)")
+    Page<CharacterModel> findAllWithFilters(
+            @Param("name") String name,
+            @Param("status") LifeStatus status,
+            @Param("species") String species,
+            @Param("characterType") String characterType,
+            @Param("gender") Gender gender,
+            Pageable pageable
+    );
 }
